780dfd9f96028cde632c5389f04cf7598276fef1
cuongdang9 <113906420+cuongdang9@users.noreply.github.com>
2025-03-07 23:43:41 +0700

    Add files via upload

---
+++ Test_RFID.ino
@@ -0,0 +1,58 @@
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+
+#define RST_PIN   22  
+#define SS_PIN    5  
+#define MISO_PIN  19
+#define MOSI_PIN  23 
+#define SCK_PIN   18
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(115200);   // Initiate a serial communication
+  SPI.begin()     //SPI.begin (SCK_PIN, MISO_PIN, MOSI_PIN);      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "51 30 CD 3B") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    delay(3000);
+  }
+} 
